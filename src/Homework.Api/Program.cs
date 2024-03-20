bdea69a62143a805955b3ea65e2effb18f7bbb1c
Maciej Kuśnierz <maciek.hooch@gmail.com>
2024-03-20 14:55:44 +0100

    Improve DI registration in Homework.Api

--- src/Homework.Api/Program.cs
+++ src/Homework.Api/Program.cs
@@ -1,23 +1,26 @@
+using Homework.Application;
+using Homework.Infrastructure;
 using Notes.Api;
 
+// Builder
 var builder = WebApplication.CreateBuilder(args);
-{
-    builder.Services.AddPresentation();
-}
+builder.Services
+    .AddPresentation()
+    .AddApplication()
+    .AddInfrastructure();
 
+// App
 var app = builder.Build();
+app.UseExceptionHandler();
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler();
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-    app.UseRouting();
-    app.MapControllers();
-
-    app.Run();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
