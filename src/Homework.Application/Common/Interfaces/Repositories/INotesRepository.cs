db4f0a7182b29c29868810d1648ab7b2520f632d
Maciej Kuśnierz <maciek.hooch@gmail.com>
2024-03-20 13:52:44 +0100

    Add Homework.Application

---
+++ src/Homework.Application/Common/Interfaces/Repositories/INotesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Notes.Domain.Notes;
+
+namespace Homework.Application.Common.Interfaces.Repositories;
+
+public interface INotesRepository
+{
+    Task<Note?> GetByIdAsync(Guid id);
+    Task<IList<Note>> ListAsync();
+    Task AddNoteAsync(Note note);
+    Task UpdateNoteAsync(Note note);
+    Task RemoveNoteAsync(Note note);
+}
